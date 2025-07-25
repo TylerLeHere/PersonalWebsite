79d1ca3be5668697016eea236d998650086404d2
Tyler Le <tyler0308gm@gmail.com>
2025-07-25 12:46:17 -0700

    Set up Skeleton for my project

---
+++ mywebsite/src/app/layout.tsx
@@ -0,0 +1,18 @@
+// This wraps ALL pages in your app
+import type React from "react"
+import type { Metadata } from "next"
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ mywebsite/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-100 via-pink-50 to-purple-50">
+      <h1>Hello, My name is Tyler</h1>
+    </div>
+  );
+}
